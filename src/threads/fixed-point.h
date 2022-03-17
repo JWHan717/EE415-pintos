d26e6803077f774303d17a634f32ebf2b003b90c
JWHan717 <joowon@DESKTOP-0N56E9D.localdomain>
2022-03-18 02:12:36 +0900

    finished fixed-point.h

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 #define F (1 << 14)
 #define INT_MAX ((1 << 31) - 1)
 #define INT_MIN (-(1 << 31))
@@ -6,7 +8,42 @@ int int_to_fp (int n) {
     return n * F;
 }
 
-int fp_to_int (int x) {
+int fp_to_int_zero (int x) {
     return x / F;
 }
 
+int fp_to_int_nearest (int x) {
+    return (x >= 0) ? (x + F/2) : (x - F/2);
+}
+
+int fp_add (int x, int y) {
+    return x + y;
+}
+
+int fp_sub (int x, int y) {
+    return x - y;
+}
+
+int fp_add_int (int x, int n) {
+    return x + n * F;
+}
+
+int fp_sub_int (int x, int n) {
+    return x - n * F;
+}
+
+int fp_mult (int x, int y) {
+    return ((int64_t) x) * y / F;
+}
+
+int fp_mult_int (int x, int n) {
+    return x * n;
+}
+
+int fp_div (int x, int y) {
+    return ((int64_t) x) * F / y;
+}
+
+int fp_div_int (int x, int n) {
+    return x / n;
+}
